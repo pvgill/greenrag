affce8ac0c98f0178ad18644671d727ff838b138
Vick Gill <vick.gill@videoexpertsgroup.com>
2026-06-07 14:25:21 -0400

    Fix PWA start_url and service worker paths for GitHub Pages

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'greenrag-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json', '/icon.svg'];
+const ASSETS = ['/greenrag/', '/greenrag/index.html', '/greenrag/manifest.json', '/greenrag/icon.svg'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
